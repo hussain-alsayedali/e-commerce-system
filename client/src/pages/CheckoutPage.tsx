9e642e534b4f46d44d3bd4fc231337f0027de14e
Hashem Almubarak <105522749+Hashim11Mub@users.noreply.github.com>
2024-04-18 16:27:23 +0300

    Making the checkout page responsive

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -1,19 +1,20 @@
-import Input from '../components/Input.tsx';
-import Content from '../components/Content.tsx';
-
+import React from 'react';
+import Input from '../components/Input';
+import Content from '../components/Content';
 
 const CheckoutPage = () => {
-  
-
   return (
-    <><div className="py-10 px-20">
-          <h1 className="text-4xl font-semibold text-purple-900">Checkout</h1>
-          <div className="flex justify-between">
-      <Input />
-      <Content />
-          </div>
+    <div className="py-10 px-4 md:px-20">
+      <h1 className="text-2xl md:text-4xl font-semibold text-purple-900">Checkout</h1>
+      <div className="flex flex-col md:flex-row justify-between items-start mt-6">
+        <div className="w-full md:w-1/2 p-4">
+          <Input />
+        </div>
+        <div className="w-full md:w-1/2 p-4">
+          <Content />
+        </div>
       </div>
-      </>
+    </div>
   );
 };
 
