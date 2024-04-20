7b53631e72635987be5859bfebbdc2e10ef7f879
MoDarw1sh <s201952590@kfupm.edu.sa>
2024-04-20 18:13:47 +0300

    adding navigation buttons and implementing routing

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { FaBars, FaShoppingBasket, FaUserCircle } from 'react-icons/fa';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
   sidebarToggle: boolean;
   setSidebarToggle: (sidebarToggle: boolean) => void;
+
 };
 
-function Navbar({ sidebarToggle, setSidebarToggle }: Props) {
-  const iconPosition = sidebarToggle ? 'flex-end' : 'flex-start';
+const Navbar = ({ sidebarToggle, setSidebarToggle }: Props) => {
+    const navigate = useNavigate();
 
+  // Event handler for the Sign In button click
+  const handleSigninClick = () => {
+    navigate('/signin'); // Use navigate to go to '/signin'
+  };
+  const handleSignupClick =() => {
+    navigate("/signup")
+  }
+  const iconPosition = sidebarToggle ? 'flex-end' : 'flex-start';
   return (
-    <nav className={`bg-nuetralpurple p-4 py-3 flex justify-between ${sidebarToggle ? "" : "md:ml-64 ml-36"}`}>
+    <nav className={`bg-lightpurple p-4 py-3 flex justify-between ${sidebarToggle ? "" : "md:ml-64 ml-36"}`}>
       <div className="flex items-center" style={{ justifyContent: iconPosition }}>
         <FaBars className="text-white me-4 relative cursor-pointer z-[2]" onClick={() => setSidebarToggle(!sidebarToggle)} />
       </div>
       <span className="text-white font-semibold">Market</span>
-
       <div className="flex items-center gap-x-2">
+  <button className="bg-purple-800 hover:bg-darkpurple text-white font-bold py-2 px-4 rounded" onClick={handleSignupClick}>
+  Sign up
+</button>
+<button className="bg-transparent hover:bg-darkpurple border-2 border-purple-800 text-white font-bold py-2 px-4 rounded" onClick={handleSigninClick} >
+  Sign in
+</button>
+
         <FaShoppingBasket className="text-white w-6 h-6 mt-1 cursor-pointer" />
         <FaUserCircle className="text-white w-6 h-6 mt-1 cursor-pointer" />
       </div>
@@ -25,6 +42,5 @@ function Navbar({ sidebarToggle, setSidebarToggle }: Props) {
 }
 
 export default Navbar;
-
 
 
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,38 +1,39 @@
 import React from 'react';
-import { FaBars, FaHeadset, FaChartBar, FaBox } from 'react-icons/fa';
+import { FaBars, FaHeadset, FaChartBar, FaBox, FaHistory, FaHeart, FaTruck } from 'react-icons/fa';
 
 type Props = {
   sidebarToggle: boolean;
+  role: 'admin' | 'user';
 };
 
-function Sidebar({ sidebarToggle }: Props) {
-  const iconPosition = sidebarToggle ? 'flex-end' : 'flex-start';
+function Sidebar({ sidebarToggle, role }: Props) {
+  const adminItems = [
+    { name: 'Products', icon: FaBox, link: "product-view" },
+    { name: 'Customer Service', icon: FaHeadset, link: "#" },
+    { name: 'Dashboard', icon: FaChartBar, link: "#" },
+  ];
+
+  const userItems = [
+    { name: 'Orders', icon: FaBox, link: "product-view" },
+    { name: 'History', icon: FaHistory, link: "#" },
+    { name: 'Favorite', icon: FaHeart , link: "#" },
+    { name: 'Order Tracker', icon: FaTruck, link: "#" },
+    { name: 'Customer Service', icon: FaHeadset, link: "#"},
+  ];
+
+  const itemsToRender = role === 'admin' ? adminItems : userItems;
 
   return (
-    <div className={`${sidebarToggle ? 'hidden' : 'block'} w-36 md:w-64 z-[2] bg-nuetralpurple fixed h-full px-4 py-2`}>
-      <div className="my-2 text-xs text-white">
-        {/* Add any other content here */}
-      </div>
+    <div className={`${sidebarToggle ? 'hidden' : 'block'} z-[2] w-36 md:w-64 bg-nuetralpurple fixed h-full px-4 py-2`}>
       <ul className="mt-3 text-xs text-white font-bold md:text-sm">
-        <li className="mb-2">
-          <a href="#" className="rounded hover:bg-gray-700 p-3 flex items-center space-x-3">
-            <FaBox className="inline-block w-6 h-6 mr-2 -mt-2" />
-            Products
-          </a>
-        </li>
-        <li className="mb-2">
-          <a href="#" className="rounded hover:bg-gray-700 p-3 flex items-center space-x-3">
-            <FaHeadset className="inline-block w-6 h-6 mr-2 -mt-2" />
-            Customer Service
-          </a>
-        </li>
-        <li className="mb-2">
-          <a href="#" className="rounded hover:bg-gray-700 p-3 flex items-center space-x-3">
-            <FaChartBar className="inline-block w-6 h-6 mr-2 -mt-2" />
-            Dashboard
-          </a>
-        </li>
-        {/* Add more menu items here */}
+        {itemsToRender.map((item) => (
+          <li key={item.name} className="mb-2">
+            <a href={item.link} className="rounded hover:bg-purple-800 p-3 flex items-center space-x-3">
+              <item.icon className="inline-block w-6 h-6 mr-2" />
+              {item.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
