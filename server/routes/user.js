f695ad00007803121b3c57aa8b30fb4580514b30
Hussain <hussain.alsayedali@gmail.com>
2024-05-14 15:25:52 +0300

    get toekn typeadded

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user");
-const { verifyTokenCustomer } = require("../middleware/auth");
+const { verifyTokenCustomer, verifyToken } = require("../middleware/auth");
 router.get(
   "/getCurrentUser",
   verifyTokenCustomer,
   userController.getCurrentUser
 );
+
+router.get("/getTokenType", verifyToken, userController.getTokenType);
 
 module.exports = router;
